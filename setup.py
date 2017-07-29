e616353b3790bd14d5b47f7c9d03577360c08283
abriosi <goncalo.faria.abreu@tecnico.ulisboa.pt>
2017-07-29 03:52:22 +0100

    package complete

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mml_gmm',
+      version='0.01',
+      description='Unsupervised cluster selection',
+      url='https://github.com/abriosi/MML_GMM/',
+      author='abriosi',
+      license='MIT',
+      packages=['mml_gmm'],
+      zip_safe=False)
