c25b66363ee7a5fac517b366786d3bfb141ae8b0
abriosi <goncalo.abreu@gmail.com>
2017-07-29 21:05:14 +0100

    site corrected on setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='mml_gmm',
       version='0.1',
       description='Unsupervised cluster selection',
-      url='https://github.com/abriosi/MML_GMM/',
+      url='https://github.com/abriosi/mml_gmm/',
       author='abriosi',
       license='MIT',
       packages=['mml_gmm'],
