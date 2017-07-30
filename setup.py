7a13f1ed7a2ebdd1f4087e040366971c379c1ca7
abriosi <goncalo.abreu@gmail.com>
2017-07-30 14:17:07 +0100

    setup.py changes

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='mml_gmm',
-      version='0.1',
-      description='Unsupervised cluster selection',
+      version='0.11',
+      description='Unsupervised cluster selection for finite guassian mixture models',
       url='https://github.com/abriosi/mml_gmm/',
       author='abriosi',
       license='MIT',
